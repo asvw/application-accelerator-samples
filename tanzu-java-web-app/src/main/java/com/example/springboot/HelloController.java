888fc8a9a9ef4877e34851388f769effb51442ea
GitLab User <glab@svmail.org>
2022-11-10 09:17:35 +0100

    v

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! WFT just happened! It is running!";
+		return "Greetings from Spring Boot + Tanzu! Another commit 10Nov";
 	}
 
 }
